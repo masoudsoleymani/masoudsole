5160cf830f7d3719d677c58af78d4a0b9cea1204
Masoud Soleymani <msoolymani@gmail.com>
2026-05-15 15:54:35 +0100

    chore: enable github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: '#',
-  // base: '/',
+  site: 'https://masoudsoleymani.github.io/',
+  base: 'masoudsole',
   vite: {
     resolve: {
       tsconfigPaths: false
