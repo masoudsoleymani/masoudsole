303f755e163ae31580d9afb5c642df8ebb1141b4
Masoud Soleymani <msoolymani@gmail.com>
2026-05-19 13:43:05 +0100

    chore: update base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://masoudsoleymani.github.io/',
-  base: '/masoudsole',
+  base: '/',
   vite: {
     resolve: {
       tsconfigPaths: false
